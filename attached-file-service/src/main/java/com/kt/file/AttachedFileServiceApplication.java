34f9568dd94bb75daa882829257c4000d3fd1841
김재우 <rlawodn6548@naver.com>
2026-01-07 10:16:51 +0900

    프로젝트 생성 - article-service - attached-file-service - comment-service - config-server - eureka-server - login-service

---
+++ attached-file-service/src/main/java/com/kt/file/AttachedFileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kt.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttachedFileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttachedFileServiceApplication.class, args);
+	}
+
+}
---
+++ attached-file-service/src/test/java/com/kt/file/AttachedFileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttachedFileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
