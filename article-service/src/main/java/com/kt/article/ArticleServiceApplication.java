d73f79ac421f3f07a0c512d712ef11e040734046
김재우 <rlawodn6548@naver.com>
2026-01-08 14:34:49 +0900

    gateway server 추가 - eureka server와 연동 성공 - article service와 연동 성공

--- article-service/src/main/java/com/kt/article/ArticleServiceApplication.java
+++ article-service/src/main/java/com/kt/article/ArticleServiceApplication.java
@@ -2,8 +2,12 @@ package com.kt.article;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@RefreshScope
+@EnableEurekaClient
 public class ArticleServiceApplication {
 
 	public static void main(String[] args) {
--- gateway-server/src/main/java/com/kt/gateway/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/kt/gateway/GatewayServerApplication.java
@@ -2,8 +2,12 @@ package com.kt.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RefreshScope
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
